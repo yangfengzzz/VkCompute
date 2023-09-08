8eefa38225ad38e17d2ff023d36eb035159aa742
yangfengzzz <yangfengzzz@hotmail.com>
2023-09-08 15:09:21 +0800

    add usd target

---
+++ usd/entry.cpp
@@ -0,0 +1,17 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include <pxr/pxr.h>
+#include <pxr/base/gf/camera.h>
+#include <pxr/base/gf/vec3f.h>
+#include <pxr/base/plug/plugin.h>
+#include <pxr/base/plug/registry.h>
+#include <pxr/usd/usd/prim.h>
+#include <pxr/usd/usd/primRange.h>
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/bboxCache.h>
+#include <pxr/usd/usdGeom/camera.h>
+#include <pxr/usd/usdGeom/metrics.h>
